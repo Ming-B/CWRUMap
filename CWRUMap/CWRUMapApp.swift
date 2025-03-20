57653611eecdd0b978f99f5c335e94db231eec75
Ming Bian <127263871+Ming-B@users.noreply.github.com>
2025-03-20 10:01:42 -0400

    Initial Commit

---
+++ CWRUMap/CWRUMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CWRUMapApp.swift
+//  CWRUMap
+//
+//  Created by Ming Bian on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct CWRUMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CWRUMapTests/CWRUMapTests.swift
@@ -0,0 +1,16 @@
+//
+//  CWRUMapTests.swift
+//  CWRUMapTests
+//
+//  Created by Ming Bian on 3/20/25.
+//
+
+import Testing
+
+struct CWRUMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
